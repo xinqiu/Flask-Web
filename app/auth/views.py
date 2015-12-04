8272e4ec029d2df5369f214de8f142e04720dd1b
xinqiu <xinqiu.94@gmail.com>
2015-12-04 22:53:01 +0800

    又一个小bug，修正了不能更改邮箱的问题

--- app/auth/views.py
+++ app/auth/views.py
@@ -136,13 +136,13 @@ def password_reset(token):
 @auth.route('/change-email', methods=['GET', 'POST'])
 @login_required
 def change_email_request():
-    form =  ChangeEmailForm()
+    form = ChangeEmailForm()
     if form.validate_on_submit():
         if current_user.verify_password(form.password.data):
             new_email = form.email.data
             token = current_user.generate_email_change_token(new_email)
-            send_email(new_email, 'Confirm your email addess',
-                       'auth/email/change_email.html',
+            send_email(new_email, 'Confirm your email address',
+                       'auth/email/change_email',
                        user=current_user, token=token)
             flash('An email with instructions to confirm your new email '
                   'address has been sent to you.')
