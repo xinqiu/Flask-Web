ea5dd3c70b37b1ac5814dca5d1b1a318c369f07b
xinqiu <xinqiu.94@gmail.com>
2015-12-06 22:48:57 +0800

    follower部分写了点

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Role, Permission, Post, Follow
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -12,7 +12,7 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,
-                Post=Post)
+                Post=Post, Follow=Follow)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
