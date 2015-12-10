e6d38e5f3832a4677c600dd0e134467a5faa92eb
xinqiu <xinqiu.94@gmail.com>
2015-12-10 22:53:53 +0800

    还在学十五章

--- config.py
+++ config.py
@@ -32,6 +32,7 @@ class TesingConfig(Config):
 	TESTING = True
 	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+	WTF_CSRF_ENABLED = False
 
 class ProducitonConfig(Config):
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
